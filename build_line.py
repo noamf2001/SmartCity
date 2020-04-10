812e97cc8cdd05efa2cd1ac2fe79b0487f0843fa
NOAMFLUSS <noamf2001@gmail.com>
2020-04-10 14:34:02 +0300

    add section class

--- build_line.py
+++ build_line.py
@@ -3,7 +3,41 @@ from building import Point
 
 
 class Section:
-    def __init__(self, start_point: Point, end_point: Point):
+    def __init__(self, start_point: Point, end_point: Point, is_steps=False):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.incline = 0
+        self.width = 0
+        self.length = 0
+        self.is_steps = is_steps
+        self.railing = False
+        self.steps_num = 0
+        self.calc_width()
+        self.calc_length()
+        self.calc_incline()
+        if self.is_steps:
+            self.count_steps()
+            self.is_railing()
+
+    def is_railing(self):
+        pass
+
+    def count_steps(self):
+        pass
+
+    def calc_length(self):
+        pass
+
+    def calc_width(self):
+        pass
+
+    def calc_incline(self):
+        pass
+
+    def describe_left_roadside(self) -> str:
+        pass
+
+    def describe_right_roadside(self) -> str:
         pass
 
     def get_section_description(self) -> str:
@@ -14,9 +48,11 @@ class Section:
         pass
 
 
-def get_objects_around_line(start_point: Point, end_point: Point):
+def get_objects_around_line(start_point: Point, end_point: Point) -> List[Point]:
     pass
 
+def filter_useful_objects(points_list):
+    pass
 
 def split_line_points(start_point: Point, end_point: Point) -> List[Section]:
     """
@@ -26,8 +62,9 @@ def split_line_points(start_point: Point, end_point: Point) -> List[Section]:
 
     :return list of sections, so that every section is one kind of description
     """
-    pass
-
+    points_list = get_objects_around_line(start_point,end_point)
+    filter_useful_objects(points_list)
+    
 
 def build_line_description(start_point: Point, end_point: Point) -> str:
     result = ""
