30803f3fec35e04ad213c3f60556cfc3310611aa
NOAMFLUSS <noamf2001@gmail.com>
2020-04-16 20:44:17 +0300

    refactor and finish Building

---
+++ Parser.py
@@ -0,0 +1,11 @@
+from Building import Section
+from Building import Point
+
+def split_to_sections(points_list) -> list:
+    """
+
+    :param start_point:
+    :param end_point:
+
+    :return list of sections, so that every section is one kind of description
+    """
--- build_turn.py
+++
@@ -1,6 +0,0 @@
-def get_objects_near_point(point, radius):
-    pass
-
-
-def build_turn_description(start_point, end_point) -> str:
-    pass
